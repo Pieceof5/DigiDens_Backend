db0f7a8a88194e83ca217095e8a86e47ca982a55
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2025-12-12 11:45:43 +0200

    Alusta Spring Boot -projekti perusrakenteella ja riippuvuuksilla.

---
+++ src/main/java/com/digidens/digidens_backend/DigidensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digidens.digidens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigidensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigidensBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digidens/digidens_backend/DigidensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digidens.digidens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigidensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
