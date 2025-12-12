06eec4e0bfb3bd9d98002b921e0fa725cd60b79a
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2025-12-12 12:27:00 +0200

    Lisää yksinkertainen User-entity perusominaisuuksilla (id, username, password, role, firstname, lastname, studentNumber, gradeLevel)

---
+++ src/main/java/com/digidens/digidens_backend/model/User.java
@@ -0,0 +1,110 @@
+package com.digidens.digidens_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String role;
+
+	@Column(nullable = false)
+	private String firstname;
+
+	@Column(nullable = false)
+	private String lastname;
+
+	@Column(nullable = true, unique = true)
+	private String studentNumber;
+
+	@Column(nullable = false)
+	private String gradeLevel;
+
+	public User() {
+	}
+
+	public User(String username, String password, String role, String firstname, String lastname, String gradeLevel) {
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.gradeLevel = gradeLevel;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
+	public String getGradeLevel() {
+		return gradeLevel;
+	}
+
+	public void setGradeLevel(String gradeLevel) {
+		this.gradeLevel = gradeLevel;
+	}
+
+}
